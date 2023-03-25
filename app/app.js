32c27a0b183ce36e62498f480bdd39981bb26e49
econuri <btjung7@gmail.com>
2023-03-25 20:04:43 +0900

    프런트단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+
 const homeRouter = require('./src/routes/home');
 
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use('/', homeRouter);
 
